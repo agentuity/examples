611a900969854ccf34362615bf0f2104fa4c6b12
Nicholas Mirigliani <nicholasmirigliani@gmail.com>
2025-12-22 14:59:37 -0500

    weather agent

--- training-v1/hello-world-ts-v1/src/api/index.ts
+++ training-v1/hello-world-ts-v1/src/api/index.ts
@@ -3,8 +3,11 @@ import hello from '@agent/hello';
 
 const api = createRouter();
 
+// Define a POST request at /hello, directed to our 'hello' agent.
 api.post('/hello', hello.validator(), async (c) => {
+	// Get valid JSON object from request.
 	const data = c.req.valid('json');
+	// Run the 'hello' agent with our JSON data.
 	const result = await hello.run(data);
 	return c.json(result);
 });
